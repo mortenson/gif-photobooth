9c6356429f0788f74fbbdd7ef1bbb215621e88ad
Samuel Mortenson <samuel@mortenson.coffee>
2017-08-08 20:53:45 -0700

    Improvements

--- js/main.js
+++ js/main.js
@@ -11,21 +11,23 @@
       prep_time: 6000,
       rows: 4,
       gutter: 7,
-      gutter_color: 'black',
+      gutter_color: '#000000',
       width: 200,
       height: 900,
       style: 'none'
     };
 
   for (var name in base_config) {
-    if (window['config_' + name]) {
-      window['config_' + name].value = base_config[name];
+    var input = document.getElementById('config_' + name);
+    if (input) {
+      input.value = base_config[name];
     }
   }
 
   function config (name) {
-    if (window['config_' + name] && window['config_' + name].value.length) {
-      var value = window['config_' + name].value;
+    var input = document.getElementById('config_' + name);
+    if (input && input.value.length) {
+      var value = input.value;
       if (name === 'gutter_color' || name === 'style') {
         return value;
       }
@@ -45,73 +47,94 @@
   }
 
   function setStatus (text, body_class) {
-    statustext.textContent = text;
-    console.log(text);
+    var status = document.getElementById('statustext');
+    status.textContent = text;
     document.body.classList = body_class || '';
+  }
+
+  function prepFrames () {
+    var frames = [], context;
+    for (var i = 0; i < config('num_frames'); ++i) {
+      frames[i] = document.createElement('canvas');
+      frames[i].width = config('width');
+      frames[i].height = config('height');
+      context = frames[i].getContext('2d');
+      context.fillStyle = config('gutter_color');
+      context.fillRect(0, 0, frames[i].width, frames[i].height);
+    }
+    return frames;
+  }
+
+  function setCountdown (i) {
+    if (config('prep_time') > 3000) {
+      var pose_time = config('frame_delay') * config('num_frames');
+      sleep((config('prep_time') * i) + (config('prep_time') - 3000) + (pose_time * i)).then(function () {
+        for (let count = 0; count < 3; ++count) {
+          sleep(1000 * count).then(function () {
+            setStatus(3 - count, 'count');
+          });
+        }
+      });
+    }
+  }
+
+  function drawPose (frame, i) {
+    var target_height = getTargetHeight();
+    var target_width = config('width');
+    var context = frame.getContext('2d');
+    var video = document.getElementById('videomirror');
+    context.drawImage(video, 0, (i * target_height) + ((i + 1) * config('gutter')), target_width, target_height);
+  }
+
+  function compileGIF (frames) {
+    var gif = new GIF({
+      workers: 2,
+      workerScript: 'js/gif.js/gif.worker.js',
+      quality: 10
+    });
+    for (var i in frames) {
+      gif.addFrame(frames[i], { delay: 200 });
+    }
+    var preview = document.getElementById('preview');
+    gif.on('finished', function (blob) {
+      if (preview.src) {
+        URL.revokeObjectURL(preview.src);
+      }
+      var url = URL.createObjectURL(blob);
+      preview.src = url;
+      var downloadlink = document.getElementById('downloadlink');
+      downloadlink.href = url;
+      downloadlink.download = 'photobooth.gif';
+      document.getElementById('preview-wrapper').classList.add('active');
+      setStatus('Click me');
+    });
+    gif.render();
+    running = false;
+    setStatus('Loading GIF');
   }
 
   function startCapture () {
     if (ready && !running) {
       setStatus('Get ready...', 'ready');
-      preview.parentElement.classList.remove('active');
+      document.getElementById('preview-wrapper').classList.remove('active');
       running = true;
-      var target_height = getTargetHeight();
-      var target_width = config('width');
-      var pose_time = config('frame_delay') * config('num_frames');
-      var base_canvas = document.createElement('canvas');
-      base_canvas.width = config('width');
-      base_canvas.height = config('height');
-      var frames = [], context, gif;
-      for (let i = 0; i < config('rows'); ++i) {
-        if (config('prep_time') > 3000) {
-          sleep((config('prep_time') * i) + (config('prep_time') - 3000) + (pose_time * i)).then(function () {
-            for (let count = 0; count < 3; ++count) {
-              sleep(1000 * count).then(function () {
-                setStatus(3 - count, 'count');
-              });
-            }
-          });
-        }
+      var num_frames = config('num_frames');
+      var frame_delay = config('frame_delay');
+      var pose_time = frame_delay * num_frames;
+      var frames = prepFrames();
+      var rows = config('rows');
+      for (let i = 0; i < rows; ++i) {
+        setCountdown(i);
         sleep((config('prep_time') * (i + 1)) + (pose_time * i)).then(function () {
-          for (let j = 0; j < config('num_frames'); ++j) {
-            sleep(config('frame_delay') * j).then(function () {
+          for (let j = 0; j < num_frames; ++j) {
+            sleep(frame_delay * j).then(function () {
               setStatus('Pose!', 'pose');
-              if (!frames[j]) {
-                frames[j] = base_canvas.cloneNode();
-                context = frames[j].getContext('2d');
-                context.fillStyle = config('gutter_color');
-                context.fillRect(0, 0, frames[j].width, frames[j].height);
-              }
-              else {
-                context = frames[j].getContext('2d');
-              }
-              context.drawImage(videomirror, 0, (i * target_height) + ((i + 1) * config('gutter')), target_width, target_height);
-              if (j === config('num_frames') - 1) {
+              drawPose(frames[j], i);
+              if (j === num_frames - 1) {
                 setStatus('Get Ready...', 'ready');
               }
-              if (i === config('rows') - 1 && j === config('num_frames') - 1) {
-                gif = new GIF({
-                  workers: 2,
-                  workerScript: 'js/gif.js/gif.worker.js',
-                  quality: 10
-                });
-                for (var k in frames) {
-                  gif.addFrame(frames[k], {delay: 200});
-                }
-                gif.on('finished', function (blob) {
-                  if (preview.src) {
-                    URL.revokeObjectURL(preview.src);
-                  }
-                  var url = URL.createObjectURL(blob);
-                  preview.src = url;
-                  downloadlink.href = url;
-                  downloadlink.download = 'photobooth.gif';
-                  preview.parentElement.classList.add('active');
-                  setStatus('Click me');
-                });
-                gif.render();
-                running = false;
-                setStatus('Loading GIF');
+              if (i ===rows - 1 && j === num_frames - 1) {
+                compileGIF(frames);
               }
             });
           }
@@ -120,9 +143,9 @@
     }
   }
 
-  statustext.onclick = startCapture;
+  document.getElementById('statustext').onclick = startCapture;
 
-  closelink.onclick = function () {
+  document.getElementById('closelink').onclick = function () {
     preview.parentElement.classList.remove('active');
   };
 
@@ -132,68 +155,78 @@
     }
   };
 
+  function drawVideoMirror (canvas) {
+    var video = document.getElementById('video');
+    var target_height = getTargetHeight();
+    var target_width = config('width');
+    var videomirror = document.getElementById('videomirror');
+    videomirror.width = target_width;
+    videomirror.height = target_height;
+    var context = videomirror.getContext('2d');
+    context.imageSmoothingEnabled = false;
+    var ratio  = Math.max(target_width  / video.videoWidth, target_height / video.videoHeight);
+    var x = (target_width - video.videoWidth * ratio) / 2;
+    var y = (target_height - video.videoHeight * ratio) / 2;
+    context.drawImage(video, 0, 0, video.videoWidth, video.videoHeight, x, y, video.videoWidth * ratio, video.videoHeight * ratio);
+    canvas.draw(canvas.texture(videomirror));
+  }
+
+  function applyStyles (canvas) {
+    var videomirror = document.getElementById('videomirror');
+    var context = videomirror.getContext('2d');
+    var style = config('style');
+    switch (style) {
+      case 'grayscale':
+        canvas.hueSaturation(-1, -1);
+        break;
+      case 'sepia':
+        canvas.sepia(1);
+        break;
+      case 'beauty':
+      case 'purikura':
+        canvas.denoise(80).brightnessContrast(.1, 0);
+        break;
+      case 'vignette':
+        canvas.vignette(0.5, 0.7);
+        break;
+      case 'motionblur':
+        canvas.zoomBlur(videomirror.width / 2, videomirror.height / 2, 0.2);
+        break;
+      default:
+        break;
+    }
+    canvas.update();
+    context.translate(videomirror.width, 0);
+    context.scale(-1, 1);
+    context.drawImage(canvas, 0, 0, videomirror.width, videomirror.height);
+    if (style === 'purikura') {
+      var purikura = document.getElementById('purikura');
+      context.drawImage(purikura.children[0], 0, 0, videomirror.width, videomirror.height);
+    }
+  }
+
   if (navigator && navigator.mediaDevices) {
-    var interval;
-    navigator.mediaDevices.getUserMedia({video: {facingMode: 'user'}}).then(function (stream) {
+    navigator.mediaDevices.getUserMedia({ video: { facingMode: 'user' } }).then(function (stream) {
+      var video = document.getElementById('video');
       video.srcObject = stream;
       video.play();
       video.addEventListener('canplay', function () {
-        if (interval) {
-          clearInterval(interval);
-        }
         var canvas = fx.canvas();
         setTimeout(function () {
           ready = true;
           setStatus('Click me');
-          interval = setInterval(function () {
-            var target_height = getTargetHeight();
-            var target_width = config('width');
-            videomirror.width = target_width;
-            videomirror.height = target_height;
-            var context = videomirror.getContext('2d');
-            context.imageSmoothingEnabled = false;
-            var ratio  = Math.max(target_width  / video.videoWidth, target_height / video.videoHeight);
-            var x = (target_width - video.videoWidth * ratio) / 2;
-            var y = (target_height - video.videoHeight * ratio) / 2;
-            context.drawImage(video, 0, 0, video.videoWidth, video.videoHeight, x, y, video.videoWidth * ratio, video.videoHeight * ratio);
-            canvas.draw(canvas.texture(videomirror));
-            var style = config('style');
-            switch (style) {
-              case 'grayscale':
-                canvas.hueSaturation(-1, -1);
-                break;
-              case 'sepia':
-                canvas.sepia(1);
-                break;
-              case 'beauty':
-              case 'purikura':
-                canvas.denoise(80).brightnessContrast(.1, 0);
-                break;
-              case 'vignette':
-                canvas.vignette(0.5, 0.7);
-                break;
-              case 'motionblur':
-                canvas.zoomBlur(videomirror.width / 2, videomirror.height / 2, 0.2);
-                break;
-              default:
-                break;
-            }
-            canvas.update();
-            context.translate(videomirror.width, 0);
-            context.scale(-1, 1);
-            context.drawImage(canvas, 0, 0, videomirror.width, videomirror.height);
-            if (style === 'purikura') {
-              context.drawImage(purikura.children[0], 0, 0, videomirror.width, videomirror.height);
-            }
-          }, 10);
+          setInterval(function () {
+            drawVideoMirror(canvas);
+            applyStyles(canvas);
+          }, 100);
         }, config('ramp_time'));
       }, false);
     }).catch(function () {
-      setStatus('Webcam issues 😭', 'error');
+      setStatus('Webcam issues. Did you deny access? 😿', 'error');
     });
   }
   else {
-    setStatus('Incompatible browser 😭', 'error');
+    setStatus('Incompatible browser. Chrome latest works! 😿', 'error');
   }
 
 }());
